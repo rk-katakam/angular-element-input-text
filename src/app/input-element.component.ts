2d9137252448672951c210fe26444d0b41fef576
Rajanikanth Katakam <Rajanikanth.Katakam@uppal.ggktech.local>
2018-09-04 19:22:02 +0530

    Added Input to Angular Element

--- src/app/input-element.component.ts
+++ src/app/input-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-element',
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class InputElement {
 
-  public inputValue:string;
+  @Input('text') inputValue:string;
+
   inputSubmit(event) {
     this.inputValue += this.inputValue; 
     console.log('inputValue', this.inputValue, event.target);
